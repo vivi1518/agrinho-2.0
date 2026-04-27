97f4d73a57d031f5e9cf62194f5c7648f9c3f3ee
Viviane Aparecida Martins Correa <correa.viviane@escola.pr.gov.br>
2026-04-27 08:16:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 const botao = document.getElementById("toggleTema");
 
-// MODO ESCURO COM SALVAMENTO
 botao.addEventListener("click", () => {
   document.body.classList.toggle("light");
-
-  if (document.body.classList.contains("light")) {
-    localStorage.setItem("tema", "light");
-  } else {
-    localStorage.setItem("tema", "dark");
-  }
 });
-
-// CARREGAR TEMA SALVO
-window.onload = () => {
-  const temaSalvo = localStorage.getItem("tema");
-
-  if (temaSalvo === "light") {
-    document.body.classList.add("light");
-  }
-};
